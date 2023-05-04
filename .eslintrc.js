574fd22a07596568fa3540eb0f3c8bff70d73e52
vinayak shahdeo <vinushahdeo3333@gmail.com>
2023-05-05 01:43:49 +0530

    changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,6 +27,7 @@ module.exports = {
 		'react/prop-types': 'off',
 		'react/jsx-uses-react': 'off',
 		'react/react-in-jsx-scope': 'off',
+		'react/no-unescaped-entities': 0,
 		'@typescript-eslint/explicit-module-boundary-types': 'off',
 	},
 };
